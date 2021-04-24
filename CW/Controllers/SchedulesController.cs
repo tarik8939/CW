604c2cd39e23ed0b03c75fb9f4f1725f7b7f1e7b
tarik8939 <39026353+tarik8939@users.noreply.github.com>
2021-04-25 01:57:58 +0300

    Added functions for SchedulesController

--- CW/Controllers/SchedulesController.cs
+++ CW/Controllers/SchedulesController.cs
@@ -23,13 +23,34 @@ namespace CW.Controllers
         public async Task<IActionResult> Index()
         {
             var cWContext = _context.Schedules
+                .Include(s => s.Route).
+                    ThenInclude(x=>x.CityFromNavigation)
                 .Include(s => s.Route)
+                    .ThenInclude(x => x.CityToNavigation)
                 .Include(s => s.Transport)
+                .ThenInclude(x=>x.Brand)
                 .Include(s => s.Worker);
             return View(await cWContext.ToListAsync());
         }
 
+        //список маршрутів для певного водія
+        [Authorize(Roles = "Admin,Driver-Intern,Driver")]
+        public async Task<IActionResult> GetSheduleForWorker(string email)
+        {
+            var route2 = await _context.Schedules.Where(x => x.Worker.Email == email)
+                .Include(x => x.Route)
+                .ThenInclude(x => x.CityFromNavigation)
+                .Include(x => x.Route)
+                .ThenInclude(x => x.CityToNavigation)
+                .Include(x => x.Transport)
+                .ThenInclude(x => x.Brand)
+                .ToListAsync();
+
+            return View("../Schedules/Schedules", route2);
+        }
+
         // GET: Schedules/Details/5
+        //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -39,6 +60,9 @@ namespace CW.Controllers
 
             var schedule = await _context.Schedules
                 .Include(s => s.Route)
+                    .ThenInclude(x => x.CityFromNavigation)
+                .Include(s => s.Route)
+                    .ThenInclude(x => x.CityToNavigation)
                 .Include(s => s.Transport)
                 .ThenInclude(x=>x.Brand)
                 .Include(s => s.Worker)
@@ -53,11 +77,15 @@ namespace CW.Controllers
 
         // GET: Schedules/Create
         [Authorize(Roles = "Admin")]
-        public IActionResult Create()
+        public IActionResult Create(Route? route)
         {
-            ViewData["RouteId"] = new SelectList(_context.Routes, "RouteId", "RouteId");
-            ViewData["TransportId"] = new SelectList(_context.Transports, "TransportId", "Description");
-            ViewData["WorkerId"] = new SelectList(_context.Workers, "WorkerId", "Email");
+            ViewData["Route"] = new SelectList(_context.Routes
+                .Include(x=>x.CityFromNavigation)
+                .Include(x=>x.CityToNavigation), "RouteId", "RouteName",route.RouteId);
+            ViewData["Transport"] = new SelectList(_context.Transports
+                .Include(x=>x.Brand), "TransportId", "TransportName");
+            ViewData["Worker"] = new SelectList(_context.Workers.Include(x=>x.Role)
+                    .Where(x=>x.Role.Role1.Contains("Driver")),"WorkerId", "WorkerFullName");
             return View();
         }
 
@@ -67,18 +95,47 @@ namespace CW.Controllers
         [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ScheduleId,WorkerId,RouteId,TransportId,StartDateTime,EndDateTime,DateAdded,DateUpdated")] Schedule schedule)
+        public async Task<IActionResult> Create(Schedule schedule)
         {
+            var r = _context.Routes.Include(x => x.CityFromNavigation)
+                .Include(x => x.CityToNavigation)
+                .FirstOrDefault(x => x.RouteId == schedule.RouteId);
+            var dist = CalcDist(r.CityFromNavigation, r.CityToNavigation);
+            schedule.EndDateTime = CalcTime(schedule, dist);
             if (ModelState.IsValid)
             {
+                schedule.DateAdded = DateTime.Now;
+                schedule.DateUpdated = DateTime.Now;
                 _context.Add(schedule);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["RouteId"] = new SelectList(_context.Routes, "RouteId", "RouteId", schedule.RouteId);
-            ViewData["TransportId"] = new SelectList(_context.Transports, "TransportId", "Description", schedule.TransportId);
-            ViewData["WorkerId"] = new SelectList(_context.Workers, "WorkerId", "Email", schedule.WorkerId);
+            ViewData["Route"] = new SelectList(_context.Routes
+                .Include(x => x.CityFromNavigation)
+                .Include(x => x.CityToNavigation), "RouteId", "RouteName", schedule.RouteId);
+            ViewData["Transport"] = new SelectList(_context.Transports
+                .Include(x => x.Brand), "TransportId", "TransportName", schedule.TransportId);
+            ViewData["Worker"] = new SelectList(_context.Workers.Include(x => x.Role)
+                .Where(x => x.Role.Role1.Contains("Driver")), "WorkerId", "WorkerFullName", schedule.WorkerId);
+
             return View(schedule);
+        }
+
+        [Authorize(Roles = "Admin")]
+        private DateTime CalcTime(Schedule schedule, double dist)
+        {
+            DateTime date = schedule.StartDateTime;
+            var time = Math.Ceiling(Convert.ToDouble(dist / 60.0));
+            date = date.AddHours(time);
+            return date;
+        }
+
+        public double CalcDist(City cityfrom, City cityto)
+        {
+            const double R = 6371;
+            double sin1 = Math.Sin((double)((cityfrom.latitude - cityto.latitude) / 2));
+            double sin2 = Math.Sin((double)((cityfrom.longitude - cityto.longitude) / 2));
+            return 2 * R * Math.Asin(Math.Sqrt(sin1 * sin1 + sin2 * sin2 * Math.Cos((double)cityfrom.latitude) * Math.Cos((double)cityto.latitude)));
         }
 
         // GET: Schedules/Edit/5
@@ -95,9 +152,11 @@ namespace CW.Controllers
             {
                 return NotFound();
             }
-            ViewData["RouteId"] = new SelectList(_context.Routes, "RouteId", "RouteId", schedule.RouteId);
-            ViewData["TransportId"] = new SelectList(_context.Transports, "TransportId", "Description", schedule.TransportId);
-            ViewData["WorkerId"] = new SelectList(_context.Workers, "WorkerId", "Email", schedule.WorkerId);
+            ViewData["Transport"] = new SelectList(_context.Transports
+                .Include(x => x.Brand), "TransportId", "TransportName", schedule.TransportId);
+            ViewData["Worker"] = new SelectList(_context.Workers.Include(x => x.Role)
+                .Where(x => x.Role.Role1.Contains("Driver")), "WorkerId", "WorkerFullName", schedule.WorkerId);
+
             return View(schedule);
         }
 
@@ -107,17 +166,24 @@ namespace CW.Controllers
         [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ScheduleId,WorkerId,RouteId,TransportId,StartDateTime,EndDateTime,DateAdded,DateUpdated")] Schedule schedule)
+        public async Task<IActionResult> Edit(int id, Schedule schedule)
         {
             if (id != schedule.ScheduleId)
             {
                 return NotFound();
             }
 
+            var s = _context.Schedules.FirstOrDefault(x => x.ScheduleId == id);
+            var r = _context.Routes.Include(x => x.CityFromNavigation)
+                .Include(x => x.CityToNavigation)
+                .FirstOrDefault(x => x.RouteId == s.RouteId);
+            var dist = CalcDist(r.CityFromNavigation, r.CityToNavigation);
+            schedule.EndDateTime = CalcTime(schedule, dist);
             if (ModelState.IsValid)
             {
                 try
                 {
+                    schedule.DateUpdated = DateTime.Now;
                     _context.Update(schedule);
                     await _context.SaveChangesAsync();
                 }
@@ -134,13 +200,15 @@ namespace CW.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["RouteId"] = new SelectList(_context.Routes, "RouteId", "RouteId", schedule.RouteId);
-            ViewData["TransportId"] = new SelectList(_context.Transports, "TransportId", "Description", schedule.TransportId);
-            ViewData["WorkerId"] = new SelectList(_context.Workers, "WorkerId", "Email", schedule.WorkerId);
+            ViewData["Transport"] = new SelectList(_context.Transports
+                .Include(x => x.Brand), "TransportId", "TransportName", schedule.TransportId);
+            ViewData["Worker"] = new SelectList(_context.Workers.Include(x => x.Role)
+                .Where(x => x.Role.Role1.Contains("Driver")), "WorkerId", "WorkerFullName", schedule.WorkerId);
             return View(schedule);
         }
 
         // GET: Schedules/Delete/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -150,9 +218,14 @@ namespace CW.Controllers
 
             var schedule = await _context.Schedules
                 .Include(s => s.Route)
+                .ThenInclude(x => x.CityFromNavigation)
+                .Include(s => s.Route)
+                .ThenInclude(x => x.CityToNavigation)
                 .Include(s => s.Transport)
+                .ThenInclude(x => x.Brand)
                 .Include(s => s.Worker)
                 .FirstOrDefaultAsync(m => m.ScheduleId == id);
+
             if (schedule == null)
             {
                 return NotFound();
@@ -178,24 +251,6 @@ namespace CW.Controllers
             return _context.Schedules.Any(e => e.ScheduleId == id);
         }
 
-        //список маршрутів для певного водія
-        [Authorize(Roles = "Admin,Driver-Intern,Driver")]
-        public async Task<IActionResult> GetSheduleForWorker(string email)
-        {
-            //var worker = await _context.Workers.FirstOrDefaultAsync(x => x.Email == email);
-            //var routes = await _context.Workers.Include(x => x.Schedules).
-            //    Where(x => x.Email == email).ToListAsync();
-            //var worker = await _context.Workers.FirstOrDefaultAsync(x => x.Email == email);
-            //var route = await _context.Schedules.Where(x => x.WorkerId == worker.WorkerId).ToListAsync();
-            var route2 = await _context.Schedules.Where(x => x.Worker.Email == email)
-                .Include(x=>x.Route)
-                .ThenInclude(x=>x.CityFromNavigation)
-                .Include(x => x.Route)
-                .ThenInclude(x => x.CityToNavigation)
-                .Include(x=>x.Transport)
-                .ToListAsync();
 
-            return View("../Schedules/Schedules", route2);
-        }
     }
 }
